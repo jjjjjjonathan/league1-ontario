ad70c0e384822b480a01bb9cee1833227e7cef9a
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2024-10-05 12:35:42 -0400

    Set up monorepo and turbopack

--- apps/client/src/utils/trpc.ts
+++ apps/client/src/utils/trpc.ts
@@ -1,4 +1,4 @@
 import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../../../server/src';
+import type { AppRouter } from '@league1-ontario/server/src';
 
 export const trpc = createTRPCReact<AppRouter>();
