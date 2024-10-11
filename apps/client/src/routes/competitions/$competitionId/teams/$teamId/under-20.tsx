4dbf2af6d282b113a142471f0db246f31e74907c
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2024-10-10 22:19:09 -0400

    Update routers to use reference date

--- apps/client/src/routes/competitions/$competitionId/teams/$teamId/under-20.tsx
+++ apps/client/src/routes/competitions/$competitionId/teams/$teamId/under-20.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { trpc } from '@/router'
-import { MinutesLineGraph } from '@/components/minutes-line-graph'
-import { PlayerList } from '@/components/player-list'
+import { createFileRoute } from '@tanstack/react-router';
+import { trpc } from '@/router';
+import { MinutesLineGraph } from '@/components/minutes-line-graph';
+import { PlayerList } from '@/components/player-list';
 
-const YOUTH_CUTOFF = '2004-01-01'
+const YOUTH_CUTOFF = 20;
 
 export const Route = createFileRoute(
   '/competitions/$competitionId/teams/$teamId/under-20',
@@ -14,32 +14,32 @@ export const Route = createFileRoute(
       teamId: Number(params.teamId),
       competitionId: Number(params.competitionId),
       youthCutoff: YOUTH_CUTOFF,
-    })
+    });
   },
-})
+});
 
 function U20Component() {
-  const params = Route.useParams()
-  const teamId = Number(params.teamId)
-  const competitionId = Number(params.competitionId)
+  const params = Route.useParams();
+  const teamId = Number(params.teamId);
+  const competitionId = Number(params.competitionId);
   const { data } = trpc.teams.getAgeGroupOverview.useQuery({
     teamId,
     competitionId,
     youthCutoff: YOUTH_CUTOFF,
-  })
+  });
 
   if (!data) {
-    return <div>no data!</div>
+    return <div>no data!</div>;
   }
 
   return (
-    <div className="flex flex-col gap-y-8">
+    <div className='flex flex-col gap-y-8'>
       <MinutesLineGraph
         data={data.minutes}
         minimumMinutes={data.minimumU20Minutes}
-        title="U-20 Minutes"
+        title='U-20 Minutes'
       />
-      <PlayerList data={data.playerList} title="U-20 Players" />
+      <PlayerList data={data.playerList} title='U-20 Players' />
     </div>
-  )
+  );
 }
